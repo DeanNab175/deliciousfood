0037944166f4ee34b389c5159938730266f82eaf
Amir Nabeebacus <amir@nleurope.com>
2020-08-23 13:23:24 +0400

    styles header menu and slider

--- app/js/script.js
+++ app/js/script.js
@@ -4,28 +4,45 @@
     $(".owl-carousel").owlCarousel({
         items: 1,
         autoplay: true,
-        loop: true
+        loop: true,
+        smartSpeed: 1500
         //autoWidth: true
         //nav: true
     });
 })(jQuery);
 
-showMainMenu();
+toggleMainMenu();
 
-function showMainMenu() {
+function toggleMainMenu() {
     const hamburgerBtn = document.querySelector('.hamburger-btn');
     const mainMenu = document.querySelector('#main-menu-nav');
+    const animationEndEvent = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
     let show = false;
+    let effect = 'animate__zoomIn';
     
-    hamburgerBtn.addEventListener('click', (e) => {
+    hamburgerBtn.addEventListener('click', () => {
         if(!show) {
             mainMenu.style.display = 'block';
+            mainMenu.classList.add(effect);
+
+            // On animation END state remove the class
+            mainMenu.addEventListener(animationEndEvent, () => {
+                mainMenu.classList.remove(effect);
+            });
+            
             show = true;
+            
         } else {
             mainMenu.style.display = 'none';
+
+            if( mainMenu.classList.contains(effect) ) {
+                mainMenu.classList.remove(effect);
+            }
+
             show = false;
         }
     });
 
 }
+
 
